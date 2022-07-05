5c926e15b9d89ad0f06e2eaaf083b6c704f4798e
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2022-07-05 18:27:56 +1000

    working on backend now, a lot to add still

--- ContactTracing/person.py
+++ ContactTracing/person.py
@@ -1,21 +1,24 @@
+import random
 from message_broker import MessageBroker
 
 
 class Person:
-    def __init__(self, mw_endpoint: str = '', personId: str = '', movement_speed: str = ''):
+    def __init__(self, mw_endpoint: str = '', personId: str = '', movement_speed: str = '', grid_size: tuple = (10, 10)):
         self._mw_endpoint = mw_endpoint
         self._personId = personId
         self.movement_speed = movement_speed
+        self._grid_size = grid_size
         self._actual_position = tuple()
         self._msgBroker = MessageBroker()
 
     # Send new location to the broker so the tracker updates
     def send_location(self):
-        self._actual_position = self._generate_position()
-        # Do stuff here to send it to middleware
+        self._generate_position()
+        # Do stuff here to send it to middleware it would be something like:
+        # self._msgBroker.basic_publish(self._actual_position, self._personId)
 
     # Will generate a new position to be sent to the broker
-    def _generate_position(self) -> tuple:
-        new_position = tuple()
-        # Generate the new random position for the tracker
-        return new_position
+    def _generate_position(self):
+        x = random.randint(0, self._grid_size[0])
+        y = random.randint(0, self._grid_size[1])
+        self._actual_position = (x, y)
