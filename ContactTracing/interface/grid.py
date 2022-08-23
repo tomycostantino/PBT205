a413b6b3f55b82650ffbd7320869c9862fe89605
tomycostantino <73667097+tomycostantino@users.noreply.github.com>
2022-08-23 16:40:01 +1000

    each grid button represents the position

--- ContactTracing/interface/grid.py
+++ ContactTracing/interface/grid.py
@@ -12,7 +12,7 @@ class Grid(tk.Frame):
         self._canvas.pack(side=tk.TOP, expand=True, fill='both')
 
         self._user_data = []
-        self.buttons = []
+        self._buttons = []
 
     def _create_canvas(self, rows, columns, data):
         # Draw the canvas grid with the buttons in it
@@ -20,24 +20,25 @@ class Grid(tk.Frame):
 
         # Lists of positions that are close contacts
         to_color = [tuple(map(int, color['position'].split(', '))) for color in data]
-
+        idx = 0
         for i in range(1, rows + 1):
             # Draw rows
             for j in range(1, columns + 1):
                 # Draw columns
                 if (i, j) in to_color:
-                    button = tkmac.Button(self._canvas, text='{}, {}'.format(i, j), bg='red',
-                                          command=self._display_info)
-                    button.grid(row=i, column=j)
+                    self._buttons.append(tkmac.Button(self._canvas, text='{}, {}'.format(i, j), bg='red',
+                                                      command=lambda c=idx: self._display_info(self._buttons[c].cget("text"))))
+                    self._buttons[idx].grid(row=i, column=j)
 
-                    self._canvas.create_window(j * 50, i * 50, window=button)
-                    self._canvas.create_window((i * 50) + 25, (j * 50) + 25, window=button, anchor='center', width=50, height=50)
+                    self._canvas.create_window(j * 50, i * 50, window=self._buttons[idx])
+                    self._canvas.create_window((i * 50) + 25, (j * 50) + 25, window=self._buttons[idx], anchor='center', width=50, height=50)
 
+                    idx += 1
                 else:
                     # Draw grey rectangle if not in color
                     self._canvas.create_rectangle(i*50, j*50, (i+1)*50, (j+1)*50, fill='grey')
 
-    def _get_index_by_position(self, position):
+    def _get_position_index(self, position):
         positions_list = [tuple(map(int, color['position'].split(', '))) for color in self._user_data]
         for p in positions_list:
             if p[0] == position[0] and p[1] == position[1]:
@@ -53,31 +54,27 @@ class Grid(tk.Frame):
         if self._canvas.winfo_children():
             for button in self._canvas.winfo_children():
                 button.destroy()
+        self._buttons = []
 
-    def _display_info(self):
+    def _display_info(self, position):
         popup = tk.Toplevel()
         popup.wm_title('Info')
-        popup.geometry('500x500')
+        popup.geometry('200x200')
 
-        canvas = tk.Canvas(popup, width=500, height=500)
-        canvas.pack(side=tk.TOP, expand=True, fill='both')
+        p_tuple = tuple(map(int, position.split(', ')))
+        index = self._get_position_index(p_tuple)
+        data = self._user_data[index]
 
-        x1 = 0
-        x2 = 500
-        y = 88
+        infected_label = tk.Label(popup, text='Infected person: {}'.format(data['infected_person']), fg='black',
+                                  font=LABEL)
+        infected_label.pack(side=tk.TOP, expand=False, anchor='center')
 
-        for data in self._user_data:
-            infected_label = tk.Label(canvas, text='Infected person: {}'.format(data['infected_person']), fg='black', font=LABEL)
-            infected_label.pack(side=tk.TOP, expand=False, anchor='center')
+        name_label = tk.Label(popup, text='Close contact: {}'.format(data['contact']), fg='black', font=LABEL)
+        name_label.pack(side=tk.TOP, expand=False, anchor='center')
 
-            name_label = tk.Label(canvas, text='Close contact: {}'.format(data['contact']), fg='black', font=LABEL)
-            name_label.pack(side=tk.TOP, expand=False, anchor='center')
+        infected_date_label = tk.Label(popup, text='Date: {}'.format(data['date']), fg='black', font=LABEL)
+        infected_date_label.pack(side=tk.TOP, expand=False, anchor='center')
 
-            infected_date_label = tk.Label(canvas, text='Date: {}'.format(data['date']), fg='black', font=LABEL)
-            infected_date_label.pack(side=tk.TOP, expand=False, anchor='center')
+        position_label = tk.Label(popup, text='Position: {}'.format(data['position']), fg='black', font=LABEL)
+        position_label.pack(side=tk.TOP, expand=False, anchor='center')
 
-            position_label = tk.Label(canvas, text='Position: {}'.format(data['position']), fg='black', font=LABEL)
-            position_label.pack(side=tk.TOP, expand=False, anchor='center')
-
-            canvas.create_line(x1, y, x2, y, fill='black')
-            y += 88
